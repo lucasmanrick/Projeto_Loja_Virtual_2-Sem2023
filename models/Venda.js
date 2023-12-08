0a26185a198f51299303e2452492cb781ca33c1f
joao <joaowasconcelos18@gmail.com>
2023-12-08 20:13:45 -0300

    Agora vai

--- models/Venda.js
+++ models/Venda.js
@@ -54,7 +54,7 @@ let carrinho = new Venda();
 carrinho.adicionarAoCarrinho(["Produto 1", "Produto 2", "Produto 3"]);
 carrinho.mostrarCarrinho();
 
-carrinho.atualizarQuantidadeDoProduto("Produto 2", 5);
+carrinho.atualizarQuantidadeDoProduto("Produto 2",5);
 carrinho.mostrarCarrinho();
 
 carrinho.excluirItem("Produto 3");
